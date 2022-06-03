4baa2e96cd0bc55b3bafea922bd3922c73f5daca
saurabh rajput <saurabhkgf2609@gmail.com>
2022-06-03 23:25:54 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,90 @@
-import './App.css'
-// UseEffectHook
-import UseEffectHook from './Practice React/UseEffectHook';
-// props
-// import Myprops from './Practice React/Props';
-// use State
-// import MyHooks from './Practice React/Hooks';
+// import { Navbar } from 'react-bootstrap';
+import "./App.css";
 
-
+let name = "harry";
 function App() {
- 
   return (
     <div>
-       {/* Props in React
-      <h1>React Props</h1>
-     <Myprops moreInfo={"mail Id is : Saurabh123@gmail.com"}></Myprops> */}
-
-     {/* Hooks in React */}
-    {/* <MyHooks></MyHooks> */}
-
-    {/* UseEffectHook  */}
-    <UseEffectHook />
-
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            Navbar
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  Link
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="/"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <a className="dropdown-item" href="/">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="/">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="/">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled">Disabled</a>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
 
 export default App;
-
-
-
--- src/Practice React/Props.js
+++
@@ -1,7 +0,0 @@
-
-function Myprops(Props){
-    return(
-        <> Hello Saurabh {Props.moreInfo}</>
-        )
-}
-export default Myprops;
