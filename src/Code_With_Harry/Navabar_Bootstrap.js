7ebdaf55b798a884e4a978f17371ef342bc706af
saurabh rajput <saurabhkgf2609@gmail.com>
2022-06-07 22:13:53 +0530

    pr

--- src/Code_With_Harry/Navabar_Bootstrap.js
+++ src/Code_With_Harry/Navabar_Bootstrap.js
@@ -23,7 +23,7 @@ function Navbar(Props) {
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="#">
-                Home
+                About
               </a>
             </li>
             <li className="nav-item">
