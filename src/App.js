d75166043301c951808553041f9001c6efc59ed7
saurabh rajput <saurabhkgf2609@gmail.com>
2022-06-08 09:53:58 +0530

    pr

--- src/App.js
+++ src/App.js
@@ -1,50 +1,37 @@
-// import { Component } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import About from "./Code_With_Harry/About";
+import Contact from "./Code_With_Harry/Contact";
+import Disable from "./Code_With_Harry/Disable";
 import Navbar from "./Code_With_Harry/Navabar_Bootstrap";
 // import PrTextform from "./Code_With_Harry/PrTextform";
 import Textform from "./Code_With_Harry/Textform";
 // import {browserRouter as router , } from "react-router-dom"
 function App() {
-  let name = "Harry";
   return (
     <>
-    <Router>
-
       {/* // All Components are Practice i did with Code with harry */}
       <div>
-        <h1>I am Saurabh Rajput Back with {name} </h1>
         {/* ************************************************** */}
         <Navbar title="NewNavbar" Dropdown=" Dropdown 1"></Navbar>
         {/* ************************************************** */}
 
-        <div className="container">
+        {/* <div className="container">
           <Textform Heading="This is my TextInput using Props" />
         </div>
-        <br />
+        <br /> */}
         {/* <div className="container">
         <PrTextform Heading= "heading"></PrTextform>
       </div> */}
       </div>
       {/* ************************************************** */}
-     
-          <Route path="/about">
-            <About></About>
-          </Route>
-          <Route path="/users">
-            <Textform></Textform>
-          </Route>
-          {/* <Route path="/">
-            <Home />
-          </Route> */}
-        
-    </Router>
+      {/* This is React router Practice*/}
+      <Routes>
+        <Route path="/" element={<About />}></Route>
+        <Route path="/textform" element={<Textform />}></Route>
+        <Route path="/disable" element={<Disable />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
     </>
   );
 }
