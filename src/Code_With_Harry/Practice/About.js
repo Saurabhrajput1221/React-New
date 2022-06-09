7d272669fc26ea6beacbbd3c84d139c4ade77a66
saurabh rajput <saurabhkgf2609@gmail.com>
2022-06-09 10:56:43 +0530

    pr

---
+++ src/Code_With_Harry/Practice/About.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default function About() {
+  let name = "Harry";
+  return (
+    <div>This is About Component...
+      <h1>I am Saurabh Rajput Back with {name} </h1>
+    </div>
+  )
+}
