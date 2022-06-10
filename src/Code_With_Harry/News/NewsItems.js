fc6f54e525579a680569d58437123a0751959933
saurabh rajput <saurabhkgf2609@gmail.com>
2022-06-10 22:06:36 +0530

    pr

--- src/Code_With_Harry/News/NewsItems.js
+++ src/Code_With_Harry/News/NewsItems.js
@@ -1,19 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
 
 export default function NewsItems(props) {
+  let {title , description , imageUrl , newsUrl, author, date , source } = props;
   return (
     <>
+    <div className="my-3">
       <div className="card">
-        <img className="card-img-top" src="..." alt="Card image cap" />
+        <div style={{display : 'flex',
+        justifyContent: 'flex-end',
+        position: 'absolute',
+        right:'0'
+        }}><span>{source}</span></div>
+
+    </div>
+        <img className="card-img-top" src={imageUrl} alt="Card image cap" />
         <div className="card-body">
-          <h5 className="card-title">{props.tilte}</h5>
+          <h5 className="card-title">{title}</h5>
           <p className="card-text">
-           {/* {props.description} */}
+           {description}
           </p>
-          <Link to="/" className="btn btn-primary">
-            Go somewhere
-          </Link>
+         <p>card text</p>
+         <a rel="noreferrer" href={newsUrl} target="-blank" className="btn btn-success">
+           Read More
+         </a>
         </div>
       </div>
     </>
