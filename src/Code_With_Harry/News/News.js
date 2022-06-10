d25152d2c9e0efc7ce736ae932278547d7b091d1
saurabh rajput <saurabhkgf2609@gmail.com>
2022-06-10 22:06:28 +0530

    pr

--- src/Code_With_Harry/News/News.js
+++ src/Code_With_Harry/News/News.js
@@ -1,17 +1,35 @@
-import React from 'react'
-import NewsItems from './NewsItems'
+import React, { useState, useEffect } from "react";
+import { propTypes } from "react-bootstrap/esm/Image";
+import NewsItems from "./NewsItems";
 
-export default function () {
+export default function News(props) {
+  const [articles, setArticles] = useState([]);
+  const [page , setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
+
+ const fetchMoreData = async () =>{
+    setPage(page +1)
+     const url = `https;//newsapi.org`
+     let data = await fetch(url);
+     let parseData = await data.json();
+     setArticles(articles.concat[parseData.articles])
+     setTotalResults(parseData.totalResults);
+  }
   return (
     <>
-    <NewsItems title="my title"></NewsItems>
-    <NewsItems></NewsItems>
-    <NewsItems description="my description"></NewsItems>
-    <NewsItems></NewsItems>
-    <NewsItems></NewsItems>
-    <NewsItems></NewsItems>
-    <NewsItems></NewsItems>
-    <NewsItems></NewsItems>
+      <NewsItems />
+
     </>
-  )
+  );
+}
+News.defaultProps = {
+  country : 'in',
+  pageSize: 8,
+  categery: 'general'
+}
+News.propTypes ={
+  country: propTypes.string,
+  pageSize: propTypes.number,
+  categery: propTypes.string
+
 }
