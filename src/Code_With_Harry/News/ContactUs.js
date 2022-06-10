f3fefdabe31f3087a3f6789b8c7ac4dbdfc455a6
saurabh rajput <saurabhkgf2609@gmail.com>
2022-06-10 22:05:59 +0530

    pr

--- src/Code_With_Harry/News/ContactUs.js
+++ src/Code_With_Harry/News/ContactUs.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function ContactUs() {
-  return (
+  const [count , Icount] = useState(0);
+  return (<>
     <div>This is ContactUs Component</div>
+    <p>UseState Practice    </p>
+    <h1>count {count}</h1>
+    <button className='btn btn-success' onClick={()=>Icount(count+ 1) }>Conting</button>
+  </>
   )
 }
