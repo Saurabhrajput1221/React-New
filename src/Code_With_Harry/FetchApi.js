76b7ab770916df59b49ad574d6cd6ab0dbfd5ee0
saurabh rajput <saurabhkgf2609@gmail.com>
2022-06-15 14:29:07 +0530

    pr

--- src/Code_With_Harry/FetchApi.js
+++ src/Code_With_Harry/FetchApi.js
@@ -1,70 +1,66 @@
 import React, { useEffect, useState } from "react";
 
 export default function FetchApi() {
-  const [Users, fetchUsers] = useState([]);
+  //   const [Users, fetchUsers] = useState([]);
+  const [PracticeApi, SetPracticeApi] = useState([]);
   //   {
   /* ******************Fetch Api******************** */
-  //   }
-  // Data will be calling again and again that's why we are us UseEffect Hook to To stop data rendering agian and again
-  //   <h6>Fetch Api </h6>;
-  /***************Without UseEffect**************************** */
-  //  fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=6163cbe259744c738cdacf330afb4d17').then(
-  //    response =>{
-  //      return response.json();
-  //    }
-  //  ).then(
-  //    data =>{
-  //    window.document(data);
-  //    }
-  //  )
-  //  console.log((5+6))
-  // document.write(6+7)
-
-  /***************With UseEffect**************************** */
-  //   useEffect(() => {
-  //     fetch(
-  //       "https://newsapi.org/v2/top-headlines?country=us&apiKey=6163cbe259744c738cdacf330afb4d17"
-  //     )
-  //       .then((resposne) => {
-  //          resposne.json();
-  //       })
-  //       .then((response) => {
-  //         console.log(resposne);
-  //         setdata(resposne);
+  {
+    /* ***************Api 1************************************ */
+  }
+  //       const getData = () => {
+  //     fetch("https://jsonplaceholder.typicode.com/users")
+  //       .then((res) => res.json())
+  //       .then((res) => {
+  //         console.log(res);
+  //         fetchUsers(res);
   //       });
+  //   };
+  //   useEffect(() => {
+  //     getData();
   //   }, []);
 
-  
-  const getData = () => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((res) => {
-        console.log(res);
-        fetchUsers(res);
-      });
-  };
-  useEffect(() => {
-    getData();
-  }, []);
+
+  {    /* ***************Api 2 Practice************************************ */  }
+ useEffect(()=>{
+     fetch('https://reqres.in/api/users?page=2')
+     .then((PrApi)=> PrApi.json())
+     .then((PrApi)=>{
+         console.log(PrApi);
+         SetPracticeApi(PrApi);
+     })
+         
+     
+ }, [])
+
   return (
     <>
       {/* <div> Fetching Api Data </div> */}
-      <ul>
-        {Users.map((item, id )=>{
-           return          <li key={id}>
-           <p> Name = {item.name}</p>
-           <h1>This is my mail Id = {item.email}</h1> 
-            <p>This is UserName = {item.username}</p>
-            <p>This is city address =  {item.address.city}</p>
-            <p>This is Zip Code =  {item.address.zipcode}</p>
-         </li>
-       })}
-      </ul>
+      {/* ***************Api 1************************************ */}
       {/* <ul>
-        {Users.map((item, i) => {
-          return <li key={i}>{item.name}</li>;
+        {Users.map((item, id) => {
+          return (
+            <li key={id}>
+              <p> Name = {item.name}</p>
+              <h1>This is my mail Id = {item.email}</h1>
+              <p>This is UserName = {item.username}</p>
+              <p>This is city address = {item.address.city}</p>
+              <p>This is Zip Code = {item.address.zipcode}</p>
+            </li>
+          );
         })}
       </ul> */}
+
+      {/* ***************Api 2 Practice************************************ */}
+      <div>
+          {PracticeApi.map((item, id)=>{
+              return(
+                  <li key={id}>
+                      {/* <p>{item.emails}</p> */}
+                  </li>
+              )
+          })}
+      </div>
     </>
   );
 }
